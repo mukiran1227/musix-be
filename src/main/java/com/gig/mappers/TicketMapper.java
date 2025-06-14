75eb4aa28e19a230bd6976e565b5ed3eee1706c7
Vamshi_Katta <vamshi.katta@cloudfirstgroup.com>
2025-06-14 14:23:19 +0530

    fixed the events issues

--- src/main/java/com/gig/mappers/TicketMapper.java
+++ src/main/java/com/gig/mappers/TicketMapper.java
@@ -2,15 +2,24 @@ package com.gig.mappers;
 
 import com.gig.dto.TicketDTO;
 import com.gig.models.Tickets;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", 
+        unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface TicketMapper {
 
-    Tickets toEntity(TicketDTO dto);
     TicketDTO toDto(Tickets entity);
-
+    
+    Tickets toEntity(TicketDTO dto);
+    
     List<TicketDTO> toDtoList(List<Tickets> entities);
+    
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateTicketFromDto(TicketDTO dto, @MappingTarget Tickets entity);
 }
